4cd9cfa26cbacaba3b5aefabad99e9f97f93a909
Saïd Tezel <saidtezel@gmail.com>
2019-12-31 10:00:33 +0000

    Firestore doc naming improved

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, request
 from firestore import *
 from pyotp import TOTP
+import requests
+import json
 import re
 import os
 import logging
 
 SLACK_TOKEN = os.environ['SLACK_TOKEN']
+SLACK_WEBHOOK = os.environ['SLACK_WEBHOOK']
 
 app = Flask(__name__)
 
@@ -29,7 +32,7 @@ def slash():
         name = command[1]
         token = command[2]
 
-        name_normal = re.sub("[^A-Za-z0-9]+", "", name)
+        name_normal = re.sub("[^A-Za-z0-9-]+", "", name)
         add_new_token(token, name_normal)
 
         payload['text'] = f'Successfully set up token for {name_normal}'
@@ -51,6 +54,25 @@ def slash():
 
     payload['text'] = 'Sorry, I did not understand your command'
     return jsonify(payload)
+
+@app.route('/sms', methods=['GET', 'POST'])
+def receive():
+    sms_body = request.values.get('Body', None)
+
+    message = {
+        'text': sms_body
+    }
+
+    response = requests.post(
+        SLACK_WEBHOOK,
+        data = json.dumps(message),
+        headers = {'Content-Type': 'application/json'}
+    )
+
+    if response.status_code != 200:
+        raise ValueError(f'Request to Slack returned an error {response.status_code}, error response: {response.text}')
+
+    return sms_body
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
